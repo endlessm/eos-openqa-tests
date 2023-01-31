ba92b523fd8532b8eb29c09e5851ab5d387362ae
Dan Nicholson <dbn@endlessos.org>
2023-01-30 22:34:33 -0700

    utils: Use wait_serial instead of random sleeps
    This is much more deterministic than hoping the prompts will show up at
    the appropriate time.

--- lib/utils.pm
+++ lib/utils.pm
@@ -55,7 +55,7 @@ sub console_root_login {
     # logged-in needles for the console we switched from, and get out
     # of sync (e.g. https://openqa.stg.fedoraproject.org/tests/1664 )
     # To avoid this, we'll sleep a few seconds before starting
-    sleep 4;
+    wait_serial('endless login: ', timeout => 10);
 
     assert_screen('text_console_login', 10);
 
@@ -63,26 +63,30 @@ sub console_root_login {
         # Log in as the live user, as that’s the only user who is passwordless.
         # We do not know the root password.
         type_string("live\n");
+        wait_serial('live@endless:~\$ ', timeout => 10);
         assert_screen('live_console', 30);
 
         # Now slip into a sudo session. On a live image this should not require
         # a password.
         type_string("sudo -i\n");
+        wait_serial('root@endless:~# ', timeout => 10);
         assert_screen('root_console', 30);
     } else {
         my $password = get_password();
 
         # If we’re not in a live session, use the standard test username.
         type_string("test\n");
-        sleep(1);
+        wait_serial('Password: ', timeout => 10);
         type_string($password . "\n");
+        wait_serial('test@endless:~\$ ', timeout => 10);
         assert_screen('test_console', 30);
 
         # Now slip into a sudo session. This will require the user’s password
         # again.
         type_string("sudo -i\n");
-        sleep(1);
+        wait_serial('\[sudo\] password for test: ', timeout => 10)
         type_string($password . "\n");
+        wait_serial('root@endless:~# ', timeout => 10);
         assert_screen('root_console', 30);
     }
 }
@@ -105,22 +109,24 @@ sub console_user_login {
     # logged-in needles for the console we switched from, and get out
     # of sync (e.g. https://openqa.stg.fedoraproject.org/tests/1664 )
     # To avoid this, we'll sleep a few seconds before starting
-    sleep 4;
+    wait_serial('endless login: ', timeout => 10);
 
     assert_screen('text_console_login', 10);
 
     if (get_var('LIVE')) {
         # Log in as the live user. They are passwordless.
         type_string("live\n");
+        wait_serial('live@endless:~\$ ', timeout => 10);
         assert_screen('live_console', 30);
     } else {
         # If we’re not in a live session, use the standard test username.
         type_string($username . "\n");
-        sleep(1);
+        wait_serial('Password: ', timeout => 10);
         type_string(get_password() . "\n");
         if ($args{set_password}) {
             type_string(get_password() . "\n");
         }
+        wait_serial($username . '@endless:~\$ ', timeout => 10);
         assert_screen('test_console', 30);
     }
 }
